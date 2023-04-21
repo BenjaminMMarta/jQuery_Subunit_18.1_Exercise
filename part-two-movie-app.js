90b39973f03def8795ada178e7cddfffb03bcc14
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-21 09:07:36 -0700

    Added files for jQuery subunit 18.1 exercises.

---
+++ part-two-movie-app.js
@@ -0,0 +1,25 @@
+//Form Submitted. Append Inputs to the DOM//
+
+//'Click' Event Listener for Input Submit Button
+$('#form-submit-button').on('click', (event) => {
+  //Prevent Page from Refreshing When Submit Button Clicked.
+  event.preventDefault();
+  //Create Variables for Movie Container, Move Title, Movie Rating, and Delete Button.
+  let $movieTitle = $('#movie-title-input').val();
+  let $movieRating = $('#movie-rating-input').val();
+  let $movieDeleteButton = $('<input type="Submit" value="Delete">');
+  let $movieContainer = $('<div></div>').addClass('movieContainer');
+  //Appends Movie Title, Movie Rating, and Delete Button to Movie and Ratings Container.
+  $movieContainer.append($movieTitle);
+  $movieContainer.append($movieRating);
+  $movieContainer.append($movieDeleteButton);
+  $('#movies-and-ratings').append($movieContainer);
+  
+  //'Click' Event Listener for Delete Button
+  $($movieDeleteButton).on('click', (event) => {
+    $(event.target).remove($movieContainer)
+    console.log("I've been clicked");
+  });
+
+});
+
